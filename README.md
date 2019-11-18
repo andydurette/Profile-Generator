5e73d81d5a889968c45b873abb3e2f77b1b492bb
Andy Durette <duretteandy@gmail.com>
2019-11-18 12:10:04 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Profile-Generator
+Assignment 7 for University of Toronto coding bootcamp.
+
+Live Demo: 
+
+This assignment focused on creating a command-line application that dynamically generates a PDF profile from a GitHub username.
---
+++ index.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+const pdfContent = require('./generateHTML.js');
+const gitHubProfile = require('./gitHubProfile.js');
+const questions = require('./question.js');
+
+// Create PDF
+const createPdf = async (answers, gitHubInfo) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const options = {
+    path: 'pdf/AndyDuretteGithubProfile.pdf',
+    format:'A4'
+};
+  await page.setContent(pdfContent.generateHTML(answers.color, gitHubInfo ));
+  await page.pdf(options);
+  await browser.close();
+}
+
+// Call data for PDF
+let gitHubDataCall = (answers) => {
+  gitHubProfile.gitHubProfile(answers,createPdf);
+}
+
+questions.colorQuestion(gitHubDataCall);
+
+/*
+function init() {}
+
+init();
+*/
